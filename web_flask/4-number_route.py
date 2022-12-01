1a83714d9017dab653e92e126a1204d50eae0c30
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-01 17:07:51 +0100

    script that starts a flask web app

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,7 +30,7 @@ def python(text="is cool"):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return "n %d is a numbe"
+    return "n %d is a number"
 
 
 if __name__ == '__main__':
