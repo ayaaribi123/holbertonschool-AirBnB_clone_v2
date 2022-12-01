ea761aac14e39a48f601e4a815765d98ff171408
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-01 16:41:57 +0100

    script that starts a flask web app

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def hbnb():
 def C(text):
     return "C " + text.replace("_", " ")
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return "Python " + text.replace("_", " ")
